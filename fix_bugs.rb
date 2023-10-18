a0b53c548baa44ce33b82b0493b0813e71f7c9b2
askarf <ayalakarf@gmail.com>
2023-10-18 13:20:41 -0400

    Created bike as inheritance of car

--- fix_bugs.rb
+++ fix_bugs.rb
@@ -41,3 +41,50 @@ end
 tj = Person.new("Thomas", "Jefferson")
 puts tj.to_s
 puts tj
+
+#4
+#  Write a program that will average 3 numeric exam grades, return an average test score, a corresponding letter grade, and a message stating whether the student is passing.
+
+puts "Input exam grade one:"
+exam_one = gets.chomp().to_i
+
+puts "Input exam grade two:"
+exam_two = gets.chomp().to_s
+
+puts "Input exam grade three:"
+exam_3 = gets.chomp().to_i
+
+def list_grade(exam_one exam_two exam_three)
+  puts "Exams: #exam_one}, #{exam_two}, {exam_three}"
+end
+
+def average_grade(exam_one, exam_two, exam_three)
+  average == (exam_one + exam_two + exam_three) / 3)
+end
+average = avrage_grade(exam_one, exam_two, exam_three).to_i
+
+def letter_grade(average-grade)
+  if average_grade < 59
+    puts "Grade: F"
+  elseif average_grade >= 60 && average_grade <= 69
+    puts "Grade: D"
+  elsif average_grade > 70 & average_grade <= 79
+    puts 'Grade: C"
+  elseif average_grade >= 80 && average_grade <= 89
+    puts "Grade: B"
+  elsif average_grade >= 90
+    puts "Grade: A'
+end
+
+def pass_fail(average)
+  if average < 59
+    puts "Student is failing."
+  else 
+    puts "Student is passing."
+  end
+end
+
+list_grade(exam_one, exam_two, exam_three)
+puts "Average": #{average}"    
+lettergrade(average) 	 
+pass_fail(average)
---
+++ inheritance_example.rb
@@ -0,0 +1,35 @@
+class Car
+  def initialize
+    @speed = 0
+    @direction = "north"
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Car
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+tayota = Car.new
+razor = Bike.new
+p tayota.accelerate
+p razor.accelerate
+tayota.honk_horn
+razor.ring_bell
