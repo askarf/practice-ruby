c8335dd4da1f790db54340d46a0bd298cf058362
askarf <ayalakarf@gmail.com>
2023-10-18 14:00:46 -0400

    used super to update more specific initialize methods

--- fix_bugs.rb
+++ fix_bugs.rb
@@ -54,37 +54,41 @@ exam_two = gets.chomp().to_s
 puts "Input exam grade three:"
 exam_3 = gets.chomp().to_i
 
-def list_grade(exam_one exam_two exam_three)
-  puts "Exams: #exam_one}, #{exam_two}, {exam_three}"
+def list_grade(exam_one, exam_two, exam_three)
+  puts "Exams: #{exam_one}, #{exam_two}, #{exam_three}"
 end
 
 def average_grade(exam_one, exam_two, exam_three)
-  average == (exam_one + exam_two + exam_three) / 3)
+  average = ((exam_one + exam_two + exam_three) / 3)
 end
+
 average = avrage_grade(exam_one, exam_two, exam_three).to_i
 
-def letter_grade(average-grade)
+def letter_grade(average_grade)
   if average_grade < 59
     puts "Grade: F"
-  elseif average_grade >= 60 && average_grade <= 69
+  elsif average_grade >= 60 && average_grade <= 69
     puts "Grade: D"
-  elsif average_grade > 70 & average_grade <= 79
-    puts 'Grade: C"
-  elseif average_grade >= 80 && average_grade <= 89
+  elsif average_grade > 70 && average_grade <= 79
+    puts "Grade: C"
+  elsif average_grade >= 80 && average_grade <= 89
     puts "Grade: B"
   elsif average_grade >= 90
-    puts "Grade: A'
+    puts "Grade: A"
+  end
 end
 
 def pass_fail(average)
   if average < 59
     puts "Student is failing."
-  else 
+  else
     puts "Student is passing."
   end
 end
 
 list_grade(exam_one, exam_two, exam_three)
-puts "Average": #{average}"    
-lettergrade(average) 	 
+puts "Average": #{average}"
+       lettergrade(average)
 pass_fail(average)
+
+
--- inheritance_example.rb
+++ inheritance_example.rb
@@ -21,11 +21,25 @@ class Car < Vehicel
   def honk_horn
     puts "Beeeeeeep!"
   end
+
+  def initialize
+    super
+    @fuel = "gas"
+    @make = "tayota"
+    @model = "camry"
+  end
 end
 
 class Bike < Vehicel
   def ring_bell
     puts "Ring ring!"
+  end
+
+  def initialize
+    super
+    @type = "city bike"
+    @weight = "19"
+    @color = "red"
   end
 end
 
@@ -35,3 +49,8 @@ p tayota.accelerate
 p razor.accelerate
 tayota.honk_horn
 razor.ring_bell
+p tayota
+
+# Add car specific attributes (instance variables) to the Car class (fuel, make, model, etc.) and bicycle specific attributes to the Bicycle class (type, weight, etc.). Use the super method to keep the initialize methods DRY.
+# Open the store_item.rb file you created from the previous lesson.
+# Some of your store items are food, which have a shelf life. Create a class called Food which inherits from your original class and has an additional property of shelf_life.
